c955aee9a1ab397552bed990abfe182e7870004b
Boris Kuipers <boris.kuipers@hva.nl>
2025-11-07 18:03:27 +0800

    test10

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,11 @@ export default async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type, Authorization, X-Requested-With"
+);
+
 
   // Handle preflight CORS
   if (req.method == "OPTIONS") {
