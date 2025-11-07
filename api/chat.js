1fa491174ec3ec3d2caf5a9d87a559fa6047d190
Boris Kuipers <boris.kuipers@hva.nl>
2025-11-07 16:45:01 +0800

    test3

--- api/chat.js
+++ api/chat.js
@@ -2,10 +2,6 @@
 
 module.exports = async (req, res) => {
   // Altijd CORS headers zetten
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   // Super simpele debug reactie
   const info = {
     method: req.method,
@@ -14,6 +10,5 @@ module.exports = async (req, res) => {
   };
 
   res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
   return res.end(JSON.stringify(info));
 };
