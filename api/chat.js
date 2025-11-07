3eddfe4a5185b48e53b9f0d49eb1f6532388ed31
Boris Kuipers <boris.kuipers@hva.nl>
2025-11-07 16:37:59 +0800

    test2

--- api/chat.js
+++ api/chat.js
@@ -1,25 +1,18 @@
 // api/chat.js
 
 module.exports = async (req, res) => {
-  // CORS headers for everything
+  // Altijd CORS headers zetten
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Just tell us what method Vercel thinks this is
+  // Super simpele debug reactie
   const info = {
     method: req.method,
-    message: "Minimal debug handler is running"
+    message: "NEW DEBUG HANDLER IS RUNNING",
+    version: "v3"
   };
 
-  // If preflight (OPTIONS), just say ok
-  if (req.method === "OPTIONS") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    return res.end(JSON.stringify(info));
-  }
-
-  // For now: allow everything else too so we can see it works
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   return res.end(JSON.stringify(info));
